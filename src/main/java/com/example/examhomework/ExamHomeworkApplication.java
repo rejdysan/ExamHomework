d2b401c0d58071d3707f043b6e26e6bf0dfceb24
rejdysan <michal.rejduga@gmail.com>
2023-02-07 11:16:56 +0100

    initial commit

---
+++ src/main/java/com/example/examhomework/ExamHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.examhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examhomework/ExamHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
