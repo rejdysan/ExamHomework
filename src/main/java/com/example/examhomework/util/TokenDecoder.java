a04e8c4f17b54670b7f51fd93f65aeb5195dfdcd
rejdysan <michal.rejduga@gmail.com>
2023-02-09 10:56:58 +0100

    Changes - small

--- src/main/java/com/example/examhomework/util/TokenDecoder.java
+++ src/main/java/com/example/examhomework/util/TokenDecoder.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @Getter
@@ -16,9 +16,9 @@ public class TokenDecoder {
     private Long id;
     private String username;
 
-    public static TokenDecoder decodeJWT(String token) throws UnsupportedEncodingException {
+    public static TokenDecoder decodeJWT(String token) {
         String b64payload = token.split("\\.")[1];
-        String jsonString = new String(Base64.decodeBase64(b64payload), "UTF-8");
+        String jsonString = new String(Base64.decodeBase64(b64payload), StandardCharsets.UTF_8);
         return new Gson().fromJson(jsonString, TokenDecoder.class);
     }
 
