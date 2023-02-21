1a2a0cc80306548d380c06c0fefaed967fd31ed5
rejdysan <michal.rejduga@gmail.com>
2023-02-21 16:15:24 +0100

    Adding readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Hello!
+Little project created in order to prepare for GreenFox final exam.
+
+Project is a simple advertisement portal where you can sell your items as sellables and bid on other items. The project
+is built with Spring framework.
+
+* registration and login process
+* authentication with JWT token
+* MySQL database
+* including user, sellable and bid entities
--- src/test/java/com/example/examhomework/service/UserServiceImpTest.java
+++ src/test/java/com/example/examhomework/service/UserServiceImpTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -34,10 +33,10 @@ public class UserServiceImpTest {
     @MockBean
     UserRepository userRepository;
 
-    @Mock
+    @MockBean
     BCryptPasswordEncoder bcrypt;
 
-    @Mock
+    @MockBean
     BindingResult validation;
 
     RegisterRequestDTO request;
